9cb207cd0fd7ebdd438c35bdb745b25aa2580560
Allison DiNapoli <adinapol@asu.edu>
2020-05-05 21:20:03 -0700

    add redux and create a temporary set temperature function to arbitrarily change the temperature

--- webapp/src/components/Temperature/TemperatureDisplay.js
+++ webapp/src/components/Temperature/TemperatureDisplay.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React from "react"; 
 
 class Temperature extends React.Component {
+  constructor(props) {
+    super(props); 
+  }
+
   render() {
     return <h1>{this.props.degrees}&deg;{this.props.scale}</h1>;
   }
---
+++ webapp/src/reducers/temperatureReducers.js
@@ -0,0 +1,24 @@
+import { SET_TEMPERATURE } from '../actions/actionTypes'; 
+
+const initialState = {
+    temperatureDisplay: {
+        temperature: 10
+    }
+}
+
+function temperatureReducer(state = initialState, action) {
+    switch(action.type) {
+        case SET_TEMPERATURE: 
+            return {
+                ...state, 
+                temperatureDisplay: {
+                    ...state.temperatureDisplay, 
+                    temperature: action.newTemperature
+                }
+            }
+        default: 
+            return state
+    }
+}
+
+export default temperatureReducer; 
