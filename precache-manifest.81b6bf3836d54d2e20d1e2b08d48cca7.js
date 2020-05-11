06307ba020b0b9a692aab7f8b8de49b3f1c94fb7
Allison DiNapoli <adinapol@asu.edu>
2020-05-11 13:12:55 -0700

    Updates

---
+++ precache-manifest.81b6bf3836d54d2e20d1e2b08d48cca7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7947d09ade45a404143d037a8ca84ab",
+    "url": "/thermometer/index.html"
+  },
+  {
+    "revision": "fbcfa9c1bde55a62f48e",
+    "url": "/thermometer/static/css/main.60d24f6d.chunk.css"
+  },
+  {
+    "revision": "71b3af9754bd1f9bbc98",
+    "url": "/thermometer/static/js/2.ace3473f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/thermometer/static/js/2.ace3473f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbcfa9c1bde55a62f48e",
+    "url": "/thermometer/static/js/main.5c314b7e.chunk.js"
+  },
+  {
+    "revision": "8468241a1a498e40383f",
+    "url": "/thermometer/static/js/runtime-main.e5da8d77.js"
+  },
+  {
+    "revision": "10f7e47e03d643322767ef44e44b3f3b",
+    "url": "/thermometer/static/media/Orbitron-Bold.10f7e47e.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.fd626c788e30e759998cf4fcdc5e64ff.js"
+  "/thermometer/precache-manifest.81b6bf3836d54d2e20d1e2b08d48cca7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/thermometer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
