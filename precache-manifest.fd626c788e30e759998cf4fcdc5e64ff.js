c9946dd00a0cabf1178aeb777b62d21b5c741305
Allison DiNapoli <adinapol@asu.edu>
2020-05-11 13:11:40 -0700

    Updates

---
+++ precache-manifest.fd626c788e30e759998cf4fcdc5e64ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f0ebba750e1ee84983b2c1708f6e7b",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "47388edfd0756c2b5a60",
+    "url": "/react-gh-pages/static/css/main.9eb5f165.chunk.css"
+  },
+  {
+    "revision": "fdb363b01d67862e8b9c",
+    "url": "/react-gh-pages/static/js/2.79900d61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gh-pages/static/js/2.79900d61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47388edfd0756c2b5a60",
+    "url": "/react-gh-pages/static/js/main.09b56395.chunk.js"
+  },
+  {
+    "revision": "037c2f626314a6a46464",
+    "url": "/react-gh-pages/static/js/runtime-main.49c76597.js"
+  },
+  {
+    "revision": "10f7e47e03d643322767ef44e44b3f3b",
+    "url": "/react-gh-pages/static/media/Orbitron-Bold.10f7e47e.ttf"
+  }
+]);
